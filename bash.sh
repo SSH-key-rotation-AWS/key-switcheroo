33877e8c03a3de14cad8fb7aee7c787147b30cba
DbarYU <dbardich@mail.yu.edu>
2023-06-15 17:19:36 -0400

    Edited pre-commit

--- bash.sh
+++ bash.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/bash
 
 
-#find python interpeter to run the git-hook
-python_path=$(which python)
+venv_py=".venv/bin/python"
 
-if [ -z "$python_path" ]; then
-    echo "Python interpreter not found"
-    exit 1
+if [ -d "$venv_py" ]; then
+    echo "Directory exists."
+    # Perform your logic here for the existing directory
+else
+    python3 -m venv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    # Perform your logic here for the non-existing directory
 fi
-
 #find current directory, and git-hooks
 current_dir=$(pwd)
 repo_dir="$current_dir/tools/git_hooks/pre-commit"
 system_dir="$current_dir/.git/hooks/pre-commit"
 
 #copy python interpeter path to pre-commit file
-echo "#!$python_path" > $system_dir
+echo "#!$venv_py" > $system_dir
 
 #copy our manual git-hook to .git/hook/pre-commit
 cat $repo_dir >> $system_dir
 
 #make pre-commit file executable
-$(chmod +x $system_dir)
+chmod +x $system_dir
 
 #$(pip install -r requirements.txt)
 
