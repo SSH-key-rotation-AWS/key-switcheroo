1b2887ce8dda6e3ff6c4c919eff79b1e18062582
Yonatan Ginsburg <yginsburg26@gmail.com>
2023-06-15 14:01:57 -0400

    add tests

---
+++ tests/ssh_test.py
@@ -0,0 +1,48 @@
+"Testing SSH connection using ssh_key_rotator.connections Client & Server"
+import unittest
+import os
+from hamcrest import assert_that, equal_to, contains_string
+from ssh_key_rotator.connections import Client, Server
+
+class TestClientConnection(unittest.IsolatedAsyncioTestCase):
+    "Connects the client to the server and runs a couple of commands"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        def __get_username()->str:
+            user_path = os.path.expanduser("~")
+            user_path_components = user_path.split("/")
+            return user_path_components[len(user_path_components)-1]
+        self.server : Server|None = None
+        self.port = 2304
+        self.username = __get_username()
+        self.ip_address = "127.0.0.1"
+        
+    async def asyncSetUp(self):
+        self.server: Server = Server(self.port)
+        await self.server.run_ssh_server()
+
+    async def asyncTearDown(self):
+        await self.server.stop_server()
+
+    def test_echo_command(self):
+        "Can the client connect and call echo?"
+        client: Client = Client(self.ip_address, self.port, self.username)
+        client.connect_via_key()
+        command = "echo Hello"
+        __stdin, stdout, __stderr = client.execute_command(command)
+        actual_output = stdout.read().decode()
+        expected_output = "Hello\n"
+        assert_that(actual_output, equal_to(expected_output))
+
+    def test_ls_la(self):
+        "Can the client connect and call ls -la?"
+        client: Client = Client(self.ip_address, self.port, self.username)
+        client.connect_via_key()
+        command = "ls -la"
+        __stdin, stdout, __stderr = client.execute_command(command)
+        actual_output = stdout.read().decode()
+        assert_that(actual_output, contains_string("var"))
+
+if __name__ == '__main__':
+    unittest.main()
