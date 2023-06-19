0c5a779c61e9d22d4b8db15e57a288d481e4679e
Yonatan Ginsburg <yginsburg26@gmail.com>
2023-06-19 16:31:21 -0400

    implement tests with function decorator

--- tests/test_ssh.py
+++ tests/test_ssh.py
@@ -1,10 +1,12 @@
 "Testing SSH connection using ssh_key_rotator.connections Client & Server"
 import unittest
+from unittest import IsolatedAsyncioTestCase
 import os
 from hamcrest import assert_that, equal_to, contains_string
 from ssh_key_rotator.connections import Client, Server
+from tests.ssh_utils import SSHTestCase, with_ssh_server
 
-class TestClientConnection(unittest.IsolatedAsyncioTestCase):
+class TestClientConnection(IsolatedAsyncioTestCase):
     "Connects the client to the server and runs a couple of commands"
 
     def __init__(self, *args, **kwargs):
@@ -14,17 +16,11 @@ class TestClientConnection(unittest.IsolatedAsyncioTestCase):
             user_path_components = user_path.split("/")
             return user_path_components[len(user_path_components)-1]
         self.server : Server|None = None
-        self.port = 2304
+        self.port = 2500
         self.username = __get_username()
         self.ip_address = "127.0.0.1"
 
-    async def asyncSetUp(self):
-        self.server: Server = Server(self.port)
-        await self.server.start()
-
-    async def asyncTearDown(self):
-        await self.server.stop()
-
+    @with_ssh_server(port=2500)
     def test_echo_command(self):
         "Can the client connect and call echo?"
         client = self.__connect_client_via_key()
@@ -34,6 +30,7 @@ class TestClientConnection(unittest.IsolatedAsyncioTestCase):
         expected_output = "Hello\n"
         assert_that(actual_output, equal_to(expected_output))
 
+    @with_ssh_server(port=2500)
     def test_ls_la(self):
         "Can the client connect and call ls -la?"
         client = self.__connect_client_via_key()
@@ -46,6 +43,3 @@ class TestClientConnection(unittest.IsolatedAsyncioTestCase):
         client: Client = Client(self.ip_address, self.port, self.username)
         client.connect_via_key()
         return client
-
-if __name__ == '__main__':
-    unittest.main()
