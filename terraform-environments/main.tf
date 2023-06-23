2419abcce05a4d4be02f4e74aaf1fddc07fb3e26
IsaacsofferYU <isoffer@mail.yu.edu>
2023-06-23 16:37:10 -0400

    got terraform working to start, creates s3 buckets

--- terraform-environments/main.tf
+++ terraform-environments/main.tf
@@ -15,20 +15,43 @@ provider "aws" {
     region = "${var.region}"
 }
 
-module "s3" {
-    source = "<path-to-S3-folder>"
-    #bucket name should be unique
-    bucket_name = "<production_bucket1>"       
-}
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.bucket_name}"
+    acl = "${var.acl_value}"   
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-090e0fc566929d98b"
-  instance_type = "t2.micro"
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+ }
 
-  tags = {
-    Name = "TeamHenrique"
-  }
-}
+
+# module "s3" {
+#     source = "<../s3_folder/bucket.tf>"
+#     #bucket name should be unique
+#     bucket_name = "<Bucket-name>"       
+# }
+
+# provider "aws" {
+#     access_key = "${var.aws_access_key}"
+#     secret_key = "${var.aws_secret_key}"
+#     region = "${var.region}"
+# }
+
+# module "s3" {
+#     source = "../s3_folder/bucket.tf"
+#     #bucket name should be unique
+#     bucket_name = "<production_bucket1>"       
+# }
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-090e0fc566929d98b"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "TeamHenrique"
+#   }
+# }
 
 
 
--- terraform-environments/variables.tf
+++ terraform-environments/variables.tf
@@ -6,20 +6,12 @@ variable "acl_value" {
 
 }
 
-variable "aws_access_key" {
+variable "aws_access_key" {}
 
-default = “<your_access_key>”
-
-}
-
-variable "aws_secret_key" {
-
-default = “<your_secret_key>”
-
- }
+variable "aws_secret_key" {}
 
 variable "region" {
 
-    default = "region"
+    default = "us-east-2"
 
 }
