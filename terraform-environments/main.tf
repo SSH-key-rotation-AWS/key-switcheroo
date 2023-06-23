3a5e2ec753791210063221f0b678cfa853f28f74
IsaacsofferYU <isoffer@mail.yu.edu>
2023-06-23 16:37:44 -0400

    got terraform working to start, creates s3 buckets

--- terraform-environments/main.tf
+++ terraform-environments/main.tf
@@ -18,40 +18,7 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket = "${var.bucket_name}"
     acl = "${var.acl_value}"   
-
-#   tags = {
-#     Name        = "My bucket"
-#     Environment = "Dev"
-#   }
  }
-
-
-# module "s3" {
-#     source = "<../s3_folder/bucket.tf>"
-#     #bucket name should be unique
-#     bucket_name = "<Bucket-name>"       
-# }
-
-# provider "aws" {
-#     access_key = "${var.aws_access_key}"
-#     secret_key = "${var.aws_secret_key}"
-#     region = "${var.region}"
-# }
-
-# module "s3" {
-#     source = "../s3_folder/bucket.tf"
-#     #bucket name should be unique
-#     bucket_name = "<production_bucket1>"       
-# }
-
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-090e0fc566929d98b"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "TeamHenrique"
-#   }
-# }
 
 
 
