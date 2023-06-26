e2ca58da640636f4b477fbdfda73e79b7e70beea
IsaacsofferYU <isoffer@mail.yu.edu>
2023-06-26 17:04:04 -0400

    now creates ec2 instance and works for s3, but creates new s3 each time

--- terraform-environments/main.tf
+++ terraform-environments/main.tf
@@ -9,18 +9,23 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+resource "random_id" "suffix" {
+  byte_length = 8
+}
 
 provider "aws" {
     region = "${var.region}"
 }
 
 resource "aws_s3_bucket" "production" {
-  bucket = "production-bucket-team-henrique"
+    bucket = "production-bucket-${random_id.suffix.hex}"
+  # bucket = "production-bucket-team-henrique"
     acl = "${var.acl_value}"   
  }
 
  resource "aws_s3_bucket" "testing" {
-  bucket = "testing-bucket-team-henrique"
+  # bucket = "testing-bucket-team-henrique"
+  bucket = "testing-bucket-${random_id.suffix.hex}"
     acl = "${var.acl_value}"   
  }
 
