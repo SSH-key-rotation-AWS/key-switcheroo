3a53b6f70770c91813f57235843513e76335d8f9
ygins <yginsburg26@gmail.com>
2023-06-26 10:49:24 -0700

    Remove key publisher rotate_keys method

--- ssh_key_rotator/publisher/key_publisher.py
+++ ssh_key_rotator/publisher/key_publisher.py
@@ -9,19 +9,15 @@ class Publisher(ABC):
     def publish_new_key(self) -> str:
         """Abstract method for publishing a new public key"""
 
-    @abstractmethod
-    def rotate_keys(self):
-        """Abstract method for rotating public keys"""
-
 
 class S3Publisher(Publisher):
     """S3 Publisher class"""
 
+    def __init__(self, bucket_name: str):
+        self.bucket_name = bucket_name
+
     def publish_new_key(self) -> str:
         return ""
-
-    def rotate_keys(self):
-        pass
 
 
 class LocalPublisher(Publisher):
@@ -30,6 +26,3 @@ class LocalPublisher(Publisher):
     def publish_new_key(self) -> str:
         _, public_key = generate_private_public_key_in_file("~/.ssh/authorized_keys")
         return public_key.decode()
-
-    def rotate_keys(self):
-        pass
