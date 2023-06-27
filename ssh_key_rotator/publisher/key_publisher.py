5a29aff0b8069d2bcff382d846ecb1fe8487b4f2
jesselanger2 <jlanger1@mail.yu.edu>
2023-06-27 13:59:41 -0400

    allow user to specify bucket name

--- ssh_key_rotator/publisher/key_publisher.py
+++ ssh_key_rotator/publisher/key_publisher.py
@@ -34,7 +34,7 @@ class S3Publisher(Publisher):
         s3_client.put_object(
             Body=public_key,
             Bucket=self.bucket_name,
-            Key=f"{self.host}/{self.user_id}/key-cert.pub",
+            Key=f"{self.host}/{self.user_id}",
         )
 
         # Store the private key on the local machine
@@ -78,7 +78,8 @@ if __name__ == "__main__":
         publisher = LocalPublisher(server, username)
         publisher.publish_new_key()
     elif data_store == "s3":
-        publisher = S3Publisher("public-keys", server, username)
+        s3_bucket_name = input("Enter a bucket name: ")
+        publisher = S3Publisher(s3_bucket_name, server, username)
         publisher.publish_new_key()
     else:
         raise ValueError('Must specify either "local" or "s3" as the data store')
