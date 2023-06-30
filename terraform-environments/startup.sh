480e5d86d38117db04c432f483292546ff77076c
akrakauer <krakauer@mail.yu.edu>
2023-06-30 11:17:57 -0400

    script runs and first few lines work, need to fix jenkins installation

--- terraform-environments/startup.sh
+++ terraform-environments/startup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
   set -x
-  #export DEBIAN_FRONTEND=noninteractive
   sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = -1;/g" /etc/needrestart/needrestart.conf
   sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
   sudo apt update && sudo apt -y upgrade
