36c5c2912e35f9b516b1cfff106012d01f51ba90
IsaacsofferYU <isoffer@mail.yu.edu>
2023-07-03 14:09:03 -0400

    cleaned up terraform code files for readability

--- terraform-environments/variables.tf
+++ terraform-environments/variables.tf
@@ -1,21 +1,9 @@
-# variable "bucket_name" {}
-
 variable "acl_value" {
-
     default = "private"
-
 }
 
-# variable "aws_access_key" {}
-
-# variable "aws_secret_key" {}
-
-# variable "credentials_directory" {}
-
 variable "region" {
-
     default = "us-east-1"
-
 }
 
 variable "versioning" {
