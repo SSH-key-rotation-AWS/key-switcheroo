fefb853de2abcefbbcbc79a1383d7722add09f0a
akrakauer <krakauer@mail.yu.edu>
2023-07-12 15:35:22 -0400

    Made separate files for other scripts

---
+++ terraform-environments/setup.groovy
@@ -0,0 +1,13 @@
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("TeamHenrique", "AWS_SSH")
+instance.setSecurityRealm(hudsonRealm)
+instance.save()
+
+def strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
