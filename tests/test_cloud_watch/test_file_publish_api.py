829a783c845a2b1db1d5fcb51cea55049ca1a05c
ygins <yginsburg26@gmail.com>
2023-07-13 16:26:47 -0400

    Moved FilePublisher into the main api, made some improvements

--- tests/test_cloud_watch/test_file_publish_api.py
+++ tests/test_cloud_watch/test_file_publish_api.py
@@ -1,15 +1,17 @@
 """
 Module Docstring: Test File Publishing API
 """
-import json
 import unittest
 import random
 import time
 import math
 from tempfile import TemporaryDirectory
 from pathlib import Path
-from hamcrest import assert_that, equal_to
-from tests.test_cloud_watch.file_metric_publisher import FileMetricPublisher
+from hamcrest import assert_that, equal_to, has_length
+from metric_system.functions.file_metric_publisher import (
+    FileMetricPublisher,
+    MetricJsonData,
+)
 from metric_system.functions.metrics import TimingMetric, CounterMetric
 
 
@@ -24,11 +26,7 @@ class PublisherAPITest(unittest.TestCase):
         )
         self.enterContext(temp_dir)
         self._file_location = Path(temp_dir.name)
-        self.publisher = FileMetricPublisher(
-            name_space="Test NameSpace",
-            instance_id="DothansMac",
-            file_location=self._file_location,
-        )
+        self.publisher = FileMetricPublisher(metric_dir=self._file_location)
         self.inc_key_count_metric = CounterMetric(
             metric_name="File Test Counter", unit="Count"
         )
@@ -42,9 +40,11 @@ class PublisherAPITest(unittest.TestCase):
         """
         self.increment_count_metric()
         self.publisher.publish_metric(self.inc_key_count_metric)
-        data = self.retrieve_json_data(self._file_location / "test_inc_count.json")
-        val = data["Metric Data"]["Value"]
-        assert_that(self.count_int, equal_to(val))
+        data: MetricJsonData | None = self.publisher.retrieve_metric_data(
+            self.inc_key_count_metric.name
+        )
+        assert data is not None
+        assert_that(self.count_int, equal_to(data.data_points[0].value))
 
     def test_time_to_gen_key(self):
         """
@@ -52,17 +52,35 @@ class PublisherAPITest(unittest.TestCase):
         """
         self.time_key()
         self.publisher.publish_metric(self.time_metric)
-        data = self.retrieve_json_data(self._file_location / "test_timeit.json")
-        val = data["Metric Data"]["Value"]
-        assert_that(self.time_to_generate_int, equal_to(math.floor(val)))
+        data = self.publisher.retrieve_metric_data(self.time_metric.name)
+        assert data is not None
+        assert_that(
+            self.time_to_generate_int, equal_to(math.floor(data.data_points[0].value))
+        )
+
+    def test_multiple_data_points(self):
+        """
+        Tests if we can publish multiple data points
+        """
+        self.increment_count_metric()
+        self.publisher.publish_metric(self.inc_key_count_metric)
+        data = self.publisher.retrieve_metric_data(self.inc_key_count_metric.name)
+        assert data is not None
+        assert_that(data.data_points, has_length(1))
+        self.increment_count_metric()
+        self.publisher.publish_metric(self.inc_key_count_metric)
+        data2 = self.publisher.retrieve_metric_data(self.inc_key_count_metric.name)
+        assert data2 is not None
+        assert_that(data2.data_points, has_length(2))
+        assert_that(data.data_points[0].value, equal_to(self.count_int))
+        assert_that(data2.data_points[1].value, equal_to(self.count_int * 2))
 
     def increment_count_metric(self):
         """
         Simulate key count metrics
         """
-        for i in range(self.count_int):
+        for _ in range(self.count_int):
             self.inc_key_count_metric.increment()
-            print("Currently incremented by: " + str(i + 1))
 
     def time_key(self):
         """
@@ -70,13 +88,6 @@ class PublisherAPITest(unittest.TestCase):
         """
         with self.time_metric.timeit():
             time.sleep(self.time_to_generate_int)
-
-    def retrieve_json_data(self, file_location: Path):
-        """
-        Retrieve JSON data from file
-        """
-        with open(file_location, "r") as file:  # pylint: disable=unspecified-encoding
-            return json.load(file)
 
 
 if __name__ == "__main__":
