fc80c0b05af373128b097d2c3ef4756fbfbdf390
yydiamen <yydiamen@mail.yu.edu>
2023-07-13 13:25:44 -0400

    add get latest tag function

--- GithubAPI_tag_manager.py
+++ GithubAPI_tag_manager.py
@@ -1,7 +1,10 @@
-from octokit import Octokit
-from octokit import webhook
-from octokit import actions
-
-repos = Octokit().repos.get_for_org(org='octokit', type='public')
-# Make an unauthenticated request for the public repositories of the octokit organization
+def find_latest_tag():
+    url = f'https://api.github.com/repos/SSH-key-rotation-AWS/team-henrique/tags'
+    response = requests.get(url, auth=(repo_owner, access_token))
+    tags = response.json()
+    if tags:
+        latest_tag = tags[0]['name']
+        return latest_tag
+    else:
+        return None
 
