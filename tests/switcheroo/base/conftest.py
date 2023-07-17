7219e4b7e221c2f501b1b498cbda27440c48e8aa
ygins <yginsburg26@gmail.com>
2023-07-17 18:24:37 -0400

    Added unit tests for the s3 datastore

--- tests/switcheroo/base/conftest.py
+++ tests/switcheroo/base/conftest.py
@@ -5,31 +5,45 @@ import random
 import json
 import uuid
 import pytest
+from mypy_boto3_s3 import S3Client
 from switcheroo.base.data_store import FileDataStore
+from switcheroo.base.data_store.s3 import S3DataStore
 from tests.switcheroo.base.resources import Person, PersonSerializer, Figure
 
 
 @pytest.fixture(name="tmp_pathlib_dir")
-def fixture_tmp_pathlib_dir()->Generator[Path, None, None]:
+def fixture_tmp_pathlib_dir() -> Generator[Path, None, None]:
     temp_dir = TemporaryDirectory()
     with temp_dir:
         yield Path(temp_dir.name)
 
-@pytest.fixture(name="file_datastore")
-def fixture_file_datastore(tmp_pathlib_dir: Path)->FileDataStore: #Standard pytest tmpdir fixture
+
+@pytest.fixture
+def file_datastore(
+    tmp_pathlib_dir: Path,
+) -> FileDataStore:  # Standard pytest tmpdir fixture
     file_ds = FileDataStore(FileDataStore.RootInfo(tmp_pathlib_dir))
     file_ds.register_serializer(Person, PersonSerializer())
     return file_ds
 
-#Utility fixtures for the Person dataclass
+
+@pytest.fixture
+def s3_datastore(s3_bucket: str) -> S3DataStore:
+    s3_ds = S3DataStore(s3_bucket)
+    s3_ds.register_serializer(Person, PersonSerializer())
+    return s3_ds
+
+
+# Utility fixtures for the Person dataclass
 
 PersonGenerator: TypeAlias = Callable[[], Person]
 PersonLocator: TypeAlias = Callable[[Person], Path]
 PersonReader: TypeAlias = Callable[[Person], Person]
 PersonPublisher: TypeAlias = Callable[[Person], None]
 
+
 @pytest.fixture(name="person_locator")
-def fixture_person_locator(tmp_pathlib_dir: Path)->PersonLocator:
+def fixture_person_locator(tmp_pathlib_dir: Path) -> PersonLocator:
     """Fixture to locate the absolute Path of where a Person is stored using the FileDataStore
 
     Args:
@@ -38,12 +52,22 @@ def fixture_person_locator(tmp_pathlib_dir: Path)->PersonLocator:
     Returns:
         PersonLocator: A function to locate the absolute path of a Person
     """
-    def _locate_person(person: Person)->Path:
-        return tmp_pathlib_dir/person.relative_loc
+
+    def _locate_person(person: Person) -> Path:
+        return tmp_pathlib_dir / person.relative_loc
+
     return _locate_person
 
+
+def _json_to_person(data_str: str) -> Person:
+    data_obj = json.loads(data_str)
+    return Person(
+        data_obj["unique_id"], data_obj["name"], data_obj["age"], data_obj["bio"]
+    )
+
+
 @pytest.fixture
-def read_person_from_file(person_locator: PersonLocator)->PersonReader:
+def read_person_from_file(person_locator: PersonLocator) -> PersonReader:
     """Reads a person from a file based on their expected location via the given PersonLocator
 
     Args:
@@ -52,60 +76,94 @@ def read_person_from_file(person_locator: PersonLocator)->PersonReader:
     Returns:
         PersonReader: A function to read a person from their expected location
     """
-    def _read_person_from_file(person: Person)->Person:
+
+    def _read_person_from_file(person: Person) -> Person:
         person_location = person_locator(person)
         with open(person_location, mode="rt", encoding="utf-8") as person_data:
-            data_obj = json.load(person_data)
-            return Person(data_obj["unique_id"], data_obj["name"], data_obj["age"], data_obj["bio"])
+            return _json_to_person(person_data.read())
+
     return _read_person_from_file
 
+
 @pytest.fixture
-def publish_person_to_file(person_locator: PersonLocator)->PersonPublisher:
+def read_person_from_s3(s3_bucket: str, s3_client: S3Client) -> PersonReader:
+    def _read_person_from_s3(person: Person) -> Person:
+        file = s3_client.get_object(Bucket=s3_bucket, Key=str(person.relative_loc))
+        file_data = file["Body"].read()
+        contents = file_data.decode("utf-8")
+        return _json_to_person(contents)
+
+    return _read_person_from_s3
+
+
+@pytest.fixture
+def publish_person_to_file(person_locator: PersonLocator) -> PersonPublisher:
     def _publish_person_to_file(person: Person):
         person_location = person_locator(person)
         with open(person_location, mode="wt", encoding="utf-8") as person_data:
             json.dump(person.__dict__, person_data)
+
     return _publish_person_to_file
 
+
+@pytest.fixture
+def publish_person_to_s3(s3_bucket: str, s3_client: S3Client) -> PersonPublisher:
+    def _publish_person_to_s3(person: Person):
+        s3_client.put_object(
+            Bucket=s3_bucket,
+            Key=str(person.relative_loc),
+            Body=json.dumps(person.__dict__),
+        )
+
+    return _publish_person_to_s3
+
+
 @pytest.fixture(scope="session", name="person_generator")
-def fixture_person_generator()->PersonGenerator:
-    names = ["Reuven","Shimon","Levi","Yehuda","Yissachar","Zevulun"]
-    colors = ["red","yellow","orange","green","blue","purple"]
-    def _person_generator()->Person:
+def fixture_person_generator() -> PersonGenerator:
+    names = ["Reuven", "Shimon", "Levi", "Yehuda", "Yissachar", "Zevulun"]
+    colors = ["red", "yellow", "orange", "green", "blue", "purple"]
+
+    def _person_generator() -> Person:
         unique_id = str(uuid.uuid4())
         name = random.choice(names)
         color = random.choice(colors)
-        age = random.randint(1,120)
+        age = random.randint(1, 120)
         return Person(unique_id, name, age, f"{name} really likes the color {color}")
+
     return _person_generator
 
+
 @pytest.fixture
-def rand_person(person_generator: PersonGenerator)->Person:
+def rand_person(person_generator: PersonGenerator) -> Person:
     return person_generator()
 
-#Utility fixtures for the Figure dataclass
-#Very similar to the Person one
+
+# Utility fixtures for the Figure dataclass
+# Very similar to the Person one
 FigureGenerator: TypeAlias = Callable[[], Figure]
 # FigureLocator: TypeAlias = Callable[[Figure], Path]
 # FigureReader: TypeAlias = Callable[[Figure], Figure]
 # FigurePublisher: TypeAlias = Callable[[Figure], None]
 
+
 @pytest.fixture(scope="session", name="figure_generator")
-def fixture_figure_generator()->FigureGenerator:
-    shapes = ["Square","Circle","Triangle"]
-    colors = ["red","yellow","orange","green","blue","purple"]
-    def _figure_generator()->Figure:
+def fixture_figure_generator() -> FigureGenerator:
+    shapes = ["Square", "Circle", "Triangle"]
+    colors = ["red", "yellow", "orange", "green", "blue", "purple"]
+
+    def _figure_generator() -> Figure:
         unique_id = str(uuid.uuid4())
         shape = random.choice(shapes)
         color = random.choice(colors)
-        size = random.randint(1,1000)
+        size = random.randint(1, 1000)
         return Figure(unique_id, shape, color, size)
+
     return _figure_generator
 
-@pytest.fixture
-def rand_figure(figure_generator: FigureGenerator)->Figure:
-    return figure_generator()
 
+@pytest.fixture
+def rand_figure(figure_generator: FigureGenerator) -> Figure:
+    return figure_generator()
 
 
 # @pytest.fixture(name="figure_locator")
@@ -136,7 +194,8 @@ def rand_figure(figure_generator: FigureGenerator)->Figure:
 #         figure_location = figure_locator(figure)
 #         with open(figure_location, mode="rt", encoding="utf-8") as person_data:
 #             data_ob = json.load(person_data)
-#             return Figure(data_ob["unique_id"], data_ob["shape"], data_ob["color"], data_ob["size"])
+#             return Figure(data_ob["unique_id"], data_ob["shape"],
+#                           data_ob["color"], data_ob["size"])
 #     return _read_person_from_file
 
 # @pytest.fixture
--- tests/switcheroo/base/test_common_datastore.py
+++ tests/switcheroo/base/test_common_datastore.py
@@ -4,26 +4,28 @@ This module utilizes the FileDataStore because it is the simplest one to use
 from pathlib import Path
 from hamcrest import assert_that, equal_to
 from tests.switcheroo.base.resources import Person, Figure, FigureSerializer
-import tests.switcheroo.base.resources.module1 as module1
-import tests.switcheroo.base.resources.module2 as module2
+import tests.switcheroo.base.resources.module1 as module1  # pylint: disable="consider-using-from-import"
+import tests.switcheroo.base.resources.module2 as module2  # pylint: disable="consider-using-from-import"
 from switcheroo.base.data_store import FileDataStore
 
-def test_ds_can_handle_different_classes(file_datastore: FileDataStore,
-                                         rand_person: Person,
-                                         rand_figure: Figure):
-    file_datastore.register_serializer(Figure, FigureSerializer())
-    file_datastore.publish(rand_person, rand_person.relative_loc)
-    file_datastore.publish(rand_figure, rand_figure.relative_loc)
 
-    read_figure = file_datastore.retrieve(rand_figure.relative_loc, Figure)
-    read_person = file_datastore.retrieve(rand_person.relative_loc, Person)
+def test_ds_can_handle_different_classes(
+    s3_datastore: FileDataStore, rand_person: Person, rand_figure: Figure
+):
+    s3_datastore.register_serializer(Figure, FigureSerializer())
+    s3_datastore.publish(rand_person, rand_person.relative_loc)
+    s3_datastore.publish(rand_figure, rand_figure.relative_loc)
+
+    read_figure = s3_datastore.retrieve(rand_figure.relative_loc, Figure)
+    read_person = s3_datastore.retrieve(rand_person.relative_loc, Person)
 
     assert_that(read_figure, equal_to(rand_figure))
     assert_that(read_person, equal_to(rand_person))
 
-def test_ds_can_handle_diff_modules_same_class_name(file_datastore: FileDataStore):
-    file_datastore.register_serializer(module1.SomeClass, module1.SomeClassSerializer())
-    file_datastore.register_serializer(module2.SomeClass, module2.SomeClassSerializer())
+
+def test_ds_can_handle_diff_modules_same_class_name(s3_datastore: FileDataStore):
+    s3_datastore.register_serializer(module1.SomeClass, module1.SomeClassSerializer())
+    s3_datastore.register_serializer(module2.SomeClass, module2.SomeClassSerializer())
 
     mod1_loc = Path("mod1")
     mod2_loc = Path("mod2")
@@ -31,11 +33,11 @@ def test_ds_can_handle_diff_modules_same_class_name(file_datastore: FileDataStor
     mod1 = module1.SomeClass("Module1")
     mod2 = module2.SomeClass("Module2")
 
-    file_datastore.publish(mod1, mod1_loc)
-    file_datastore.publish(mod2, mod2_loc)
+    s3_datastore.publish(mod1, mod1_loc)
+    s3_datastore.publish(mod2, mod2_loc)
 
-    retrieved_mod1 = file_datastore.retrieve(mod1_loc, module1.SomeClass)
-    retrieved_mod2 = file_datastore.retrieve(mod2_loc, module2.SomeClass)
+    retrieved_mod1 = s3_datastore.retrieve(mod1_loc, module1.SomeClass)
+    retrieved_mod2 = s3_datastore.retrieve(mod2_loc, module2.SomeClass)
     assert retrieved_mod1 is not None
     assert retrieved_mod2 is not None
     assert_that(retrieved_mod1.value, equal_to(mod1.value))
