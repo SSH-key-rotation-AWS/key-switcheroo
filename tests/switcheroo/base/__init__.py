72e44b4910004289c0aa6a38ee8c87237e2b81ed
ygins <yginsburg26@gmail.com>
2023-07-17 16:45:07 -0400

    Document the file permissions matcher

--- tests/switcheroo/base/__init__.py
+++ tests/switcheroo/base/__init__.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 import uuid
 import json
 from hamcrest.core.base_matcher import BaseMatcher
+from hamcrest.core.description import Description
 from switcheroo.base.data_store import Serializer
 
 
@@ -41,8 +42,15 @@ class PersonSerializer(Serializer[Person]):
         return Person(data_obj["name"], data_obj["age"], data_obj["bio"], data_obj["unique_id"])
 
 class HasFileMode(BaseMatcher[Path]):
-
+    """Hamcrest matcher to check the file mode of a given pathlib.Path
+    """
     def __init__(self, file_mode: int) -> None:
+        """Creates the matcher
+
+        Args:
+            file_mode (int): the file mode to check for - the check treats the int as a \
+            octal value - ie if 755 is passed in, the matcher looks for the permission 0o755
+        """
         super().__init__()
         self._file_mode = file_mode
 
@@ -50,3 +58,7 @@ class HasFileMode(BaseMatcher[Path]):
         file_mode_str = oct(item.stat().st_mode) #Returns 0o100[mode] as a str
         file_mode = int(file_mode_str[5:]) #Get just [mode]
         return file_mode == self._file_mode
+
+    def describe_to(self, description: Description) -> None:
+        description.append_text(f"File mode which is {self._file_mode} in octal") \
+                   .append_text(f"(0o{self._file_mode})")
