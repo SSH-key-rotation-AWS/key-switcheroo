b6d093ea62fead4888aae802b502d7aa71a3a7f5
DbarYU <dbardich@mail.yu.edu>
2023-07-18 10:41:33 -0700

    Small test bug fixed

--- tests/test_cloud_watch/cloudwatch/test_metrics.py
+++ tests/test_cloud_watch/cloudwatch/test_metrics.py
@@ -1,24 +1,29 @@
 import unittest
 import random
 import time
+import math
 from hamcrest import assert_that, equal_to
 from metric_system.functions.metrics import TimingMetric, CounterMetric
 
+
 class MetricsFuncTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.counter_metric = CounterMetric("TEST METRIC",unit="Count")
-        self.time_metric = TimingMetric("Test Metric",unit="Seconds")
-        self.random_count = random.randint(1,10)
-        self.ramdom_time = random.randint(1,10)
+        self.counter_metric = CounterMetric("TEST METRIC", unit="Count")
+        self.time_metric = TimingMetric("Test Metric", unit="Seconds")
+        self.random_count = random.randint(1, 10)
+        self.ramdom_time = random.randint(1, 10)
 
     def test_counter_metric(self):
         for _ in range(self.random_count):
-            self.counter_metric.increment()      
-        assert_that(self.random_count,equal_to(self.counter_metric.value))
+            self.counter_metric.increment()
+        assert_that(self.random_count, equal_to(self.counter_metric.value))
+
     def test_time_metric(self):
         with self.time_metric.timeit():
             print("In Timing Metric")
             time.sleep(self.ramdom_time)
-        assert_that(self.random_count,equal_to(self.time_metric.value))  
+        assert_that(self.random_count, equal_to(math.floor(self.time_metric.value)))
+
+
 if __name__ == "__main__":
     unittest.main()
